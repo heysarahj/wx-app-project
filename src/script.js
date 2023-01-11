8661e7c76923c07ee291327fef5ac32c89905c1f
heysarahj <117777730+heysarahj@users.noreply.github.com>
2023-01-10 23:34:10 -0500

    Added Search Engine

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 function formatDate(timestamp) {
+  
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
@@ -23,13 +24,15 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-
+  console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+
 
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
@@ -37,12 +40,27 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
 }
-let apiKey="014840f60a7d32e2b13ab6867478b78a";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
+function search(city){
+  let apiKey="014840f60a7d32e2b13ab6867478b78a";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit"), handleSubmit;
+
 /*
 function formatDate(timestamp) {
   let date = new Date(timestamp);
